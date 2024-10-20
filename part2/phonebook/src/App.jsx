5b695c7cddc29a096090214cc2592cf0ef2135e5
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2024-10-20 16:04:10 -0300

    phonebook part2 axios jsonserver ej 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Filter } from './components/Filter'
 import { PersonForm } from './components/PersonForm'
 import { Persons } from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '2222222'
-     }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchPerson, setSearchPerson] = useState('')
 
-  
+  useEffect(()=>{
+      axios
+        .get('http://localhost:3001/persons')
+        .then(response => setPersons(response.data))
+  },[])
 
   const addPerson = (event) => {
     event.preventDefault()
