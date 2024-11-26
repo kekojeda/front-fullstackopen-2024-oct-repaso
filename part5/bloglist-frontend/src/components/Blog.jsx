d06cd0c3aade1909712dec6a776239af4fda1778
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2024-11-26 07:14:25 -0300

    part 5 ej 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div style={{border: "solid 1px", borderRadius: "2px", padding:"4px", margin:"3px"}}>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [detailEnabled, setDetailEnabled] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const ButtonHide = () => (
+    <button onClick={() => setDetailEnabled(!detailEnabled)}>
+      {detailEnabled ? 'Hide' : 'Show'}
+    </button>
+  )
+
+  return (
+
+    <div style={blogStyle}>
+      <p>{blog.title} <ButtonHide /></p>
+      {
+        detailEnabled && (
+          <div>
+            <h4>Blog Detail</h4>
+            <p>URL: {blog.url}</p>
+            <p>Likes: 0 <button>Like</button></p>
+            <p>Author: {blog.author}</p>
+            
+          </div>
+        )
+      }
+
+    </div>
+  )
+}
 
 export default Blog
