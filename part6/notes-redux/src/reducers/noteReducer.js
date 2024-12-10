b9ee3c1729959ac063dadc4074b49fc3a4233586
kekojeda <kekojeda@gmail.com>
2024-12-10 16:09:51 -0300

    part6 notas redux useselector

--- part6/notes-redux/src/reducers/noteReducer.js
+++ part6/notes-redux/src/reducers/noteReducer.js
@@ -20,40 +20,25 @@ const noteReducer = (state = [], action) => {
     }
   }
 
-  
-  
-  const store = createStore(noteReducer)
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'the app state is in redux store',
-      important: true,
-      id: 1
-    }
-  })
-  
-  store.dispatch({
-    type: 'NEW_NOTE',
-    payload: {
-      content: 'state changes are made with actions',
-      important: false,
-      id: 2
-    }
-  })
-  
-  const App = () => {
-    return(
-      <div>
-        <ul>
-          {store.getState().map(note=>
-            <li key={note.id}>
-              {note.content} <strong>{note.important ? 'important' : ''}</strong>
-            </li>
-          )}
-          </ul>
-      </div>
-    )
-  }
+  const generateId = () =>
+    Number((Math.random() * 1000000).toFixed(0))
 
-  export { noteReducer }
+  export const createNote = (content) => {
+    return {
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    }
+  }
+  
+  export const toggleImportanceOf = (id) => {
+    return {
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    }
+  }
+  
+  export default noteReducer
