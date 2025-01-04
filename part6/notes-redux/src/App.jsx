42b63655397ff68333728b14b78b9eb223aedc42
Sergio Ojeda <kekojeda@gmail.com>
2025-01-04 17:35:19 -0300

    notes jsonserver

--- part6/notes-redux/src/App.jsx
+++ part6/notes-redux/src/App.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
 import NewNote from "./newNote"
 import Notes from "./Notes"
 import VisibilityFilter from "./VisibilityFilter"
 
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 const App = () => {
 
-  const filterSelected = (value) => {
-    console.log(value)
-  }
+  // const filterSelected = (value) => {
+  //   console.log(value)
+  // }
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService
+      .getAll().then(notes => dispatch(setNotes(notes)))
+  }, [])
 
     return (
       <div>
--- part6/notes-redux/src/main.jsx
+++ part6/notes-redux/src/main.jsx
@@ -8,6 +8,8 @@ import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
+
+
 const store = configureStore({
   reducer: {
     notes: noteReducer,
